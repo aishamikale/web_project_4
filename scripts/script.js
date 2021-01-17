e207cc87ad771fa2ab912fe2ecb9d352aa2498f2
Aisha Gaines <aishagaines1997@gmail.com>
2021-01-16 22:29:57 -0600

    JS open and close modal, submit form, html format

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,31 @@
-const editButton = document.querySelector('.profile__edit-button'); //selector of the element you want to find
+const editButton = document.querySelector('.profile__edit-button');
+const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.modal__button');
 const form = document.querySelector('.form');
-const modal = document.querySelector('.modal');
-const nameInput = document.querySelector('.form__input_type_name');
-const titleInput = document.querySelector('.form__input_type_title');
-const profileName = document.querySelector('.profile__title');
-const profileTitle = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.form__input_type_name');
+let titleInput = document.querySelector('.form__input_type_title');
+let profileName = document.querySelector('.profile__title');
+let profileTitle = document.querySelector('.profile__subtitle');
 
-//open and closes the modal
-function toggleModal() {
-    modal.classList.toggle('modal_open');
+function openModal() {
+    modal.classList.add('modal_open');
+    nameInput.value = profileName.textContent;
+    titleInput.value = profileTitle.textContent;
 }
 
-editButton.addEventListener('click', toggleModal)
+editButton.addEventListener('click', openModal);
 
-closeButton.addEventListener('click', toggleModal)
+function closeModal() {
+    modal.classList.remove('modal_open');
+}
 
-//Changes and submits name and title info
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); //prevent default behavior of refreshing page after clicking submit
+closeButton.addEventListener('click', closeModal);
 
+function submitForm(event) {
+    event.preventDefault();
     profileName.textContent = nameInput.value;
     profileTitle.textContent = titleInput.value;
+    modal.classList.remove('modal_open');
+}
 
-    toggleModal();
-})
+form.addEventListener('submit', submitForm);
