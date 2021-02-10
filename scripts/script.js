b9853d3c502d6e515d17bad8b4e07bf5486e613e
Aisha Gaines <aishagaines1997@gmail.com>
2021-02-09 20:01:45 -0600

    1st review

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,15 @@
 const editProfileModal = document.querySelector('.modal_type_edit-profile');
 const cardModal = document.querySelector('.modal_type_add-card');
 const imageModal = document.querySelector('.modal_type_image');
+const openedPopup = document.querySelector('.modal_open');
+const popups = document.querySelectorAll('.modal');
 
 //open buttons
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 
 //close buttons
+const crossButton = document.querySelector('.modal__button');
 const closeButton = document.querySelector('.modal__button');
 const closeCardModalButton = cardModal.querySelector('.modal__button');
 const closeImageModalButton = imageModal.querySelector('.modal__image-button');
@@ -32,9 +35,18 @@ const modalImageTitle = imageModal.querySelector('.modal__image-title');
 
 
 //edit profile modal
+
 function toggleModalWindow(modal) {
-  modal.classList.toggle('modal_open');
+  modal.classList.toggle("modal_open");
+  if (modal.classList.contains("modal_open")) {
+    document.addEventListener("keydown", closeByEscape);
+  } else {
+    document.removeEventListener("keydown", closeByEscape);
+  }
 }
+/*function toggleModalWindow(modal) {
+  modal.classList.toggle('modal_open');
+}*/
 
 function submitEditProfileForm(event) {
   event.preventDefault();
@@ -71,15 +83,34 @@ function escModal(modalClose) {
   modalClose.classList.remove("modal_open");
 }
 
-document.addEventListener('keyup', (evt) => {
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    toggleModalWindow(openedPopup);
+  }
+}
+ 
+/*document.addEventListener('keyup', (evt) => {
   if (evt.key === 'Escape') {
     escModal(imageModal);
     escModal(editProfileModal);
     escModal(cardModal);
   }
-});
+});*/
 
-editProfileModal.addEventListener('click', (evt) => {
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('modal_open')) {
+        toggleModalWindow(popup)
+      }
+      if (evt.target.classList.contains('modal__button')) {
+        toggleModalWindow(popup)
+      }
+  })
+})
+
+
+
+/*editProfileModal.addEventListener('click', (evt) => {
   if(evt.target === editProfileModal) {
     escModal(editProfileModal);
   }
@@ -95,7 +126,7 @@ imageModal.addEventListener('click', (evt) => {
   if(evt.target === imageModal) {
     escModal(imageModal);
   }
-});
+});*/
  
 //initial cards
 const initialCards = [
