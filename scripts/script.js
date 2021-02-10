7df5744fbdd489d4dde27d9278f89c1607084e5d
Aisha Gaines <aishagaines1997@gmail.com>
2021-02-10 13:20:29 -0600

    review 3

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,13 @@
 const editProfileModal = document.querySelector('.modal_type_edit-profile');
 const cardModal = document.querySelector('.modal_type_add-card');
 const imageModal = document.querySelector('.modal_type_image');
-const openedPopup = document.querySelector('.modal_open');
-const popups = document.querySelectorAll('.modal');
+
 
 //open buttons
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 
 //close buttons
-const crossButton = document.querySelector('.modal__button');
 const closeButton = document.querySelector('.modal__button');
 const closeCardModalButton = cardModal.querySelector('.modal__button');
 const closeImageModalButton = imageModal.querySelector('.modal__image-button');
@@ -35,27 +33,9 @@ const modalImageTitle = imageModal.querySelector('.modal__image-title');
 
 
 //edit profile modal
-//(modal.classList.contains("modal_open"))
-
-function closeByEscape(evt) {
-  if (evt.key === 'Escape') {
-    toggleModalWindow(openedPopup);
-  }
-}
-
 function toggleModalWindow(modal) {
-  modal.classList.toggle("modal_open");
-  if (evt.target.classList.contains('modal_open') || evt.target.classList.contains('modal__button')) {
-    document.addEventListener("keydown", closeByEscape);
-  } else {
-    document.removeEventListener("keydown", closeByEscape);
-  }
-}
-
-
-/*function toggleModalWindow(modal) {
   modal.classList.toggle('modal_open');
-}*/
+}
 
 function submitEditProfileForm(event) {
   event.preventDefault();
@@ -70,51 +50,38 @@ editButton.addEventListener('click', () => {
   toggleModalWindow(editProfileModal);
 });
 
-/*closeButton.addEventListener('click', () => {
+closeButton.addEventListener('click', () => {
   toggleModalWindow(editProfileModal);
-});*/
+});
 
 addButton.addEventListener('click', () => {
   toggleModalWindow(cardModal);
 });
 
-/*closeCardModalButton.addEventListener('click', () => {
+closeCardModalButton.addEventListener('click', () => {
   toggleModalWindow(cardModal);
 });
 
 closeImageModalButton.addEventListener('click', () => {
   toggleModalWindow(imageModal);
-});*/
+});
 
 editProfileForm.addEventListener('submit', submitEditProfileForm);
 
-/*function escModal(modalClose) {
+function escModal(modalClose) {
   modalClose.classList.remove("modal_open");
-}*/
+}
 
  
-/*document.addEventListener('keyup', (evt) => {
+document.addEventListener('keyup', (evt) => {
   if (evt.key === 'Escape') {
     escModal(imageModal);
     escModal(editProfileModal);
     escModal(cardModal);
   }
-});*/
+});
 
-popups.forEach((popup) => {
-  popup.addEventListener('click', (evt) => {
-      if (evt.target.classList.contains('modal_open')) {
-        toggleModalWindow(popup);
-      }
-      if (evt.target.classList.contains('modal__button')) {
-        toggleModalWindow(popup);
-      }
-  })
-})
-
-
-
-/*editProfileModal.addEventListener('click', (evt) => {
+editProfileModal.addEventListener('click', (evt) => {
   if(evt.target === editProfileModal) {
     escModal(editProfileModal);
   }
@@ -130,7 +97,7 @@ imageModal.addEventListener('click', (evt) => {
   if(evt.target === imageModal) {
     escModal(imageModal);
   }
-});*/
+});
  
 //initial cards
 const initialCards = [
